202aab3038410f2486f05f597690e07be0a635dc
Théo Devaucoup <theo.devaucoup@pm.me>
2025-07-24 17:19:15 +0200

    fix test fail alone since code coverage

--- test/src/domrecycler.cpp
+++ test/src/domrecycler.cpp
@@ -2,10 +2,14 @@
 
 #include "wasm-dom.hpp"
 
+#include "utils.hpp"
+
 #include <emscripten.h>
 
 TEST_CASE("domRecycler", "[domRecycler]")
 {
+    setupDom();
+
     EM_ASM({
         Module.recycler.nodes = {};
         globalThis.recycler = Module.recycler;
@@ -70,7 +74,7 @@ TEST_CASE("domRecycler", "[domRecycler]")
         REQUIRE(recycler["nodes"]["DIV"]["length"].as<int>() == 0);
     }
 
-    SECTION("should create and nodes with namespace")
+    SECTION("should create nodes with namespace")
     {
         emscripten::val node = recycler.call<emscripten::val>("createNS", std::string("svg"), std::string("http://www.w3.org/2000/svg"));
         REQUIRE(node["nodeName"].as<std::string>() == "svg");
