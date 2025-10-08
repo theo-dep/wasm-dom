e4f576f42c069ce233ea1fe8ea484bdbbe5e41ac
Théo Devaucoup <theo.devaucoup@pm.me>
2025-10-08 21:27:54 +0200

    add options to control compilation flags

--- cmake/disable_exceptions.cmake
+++ cmake/disable_exceptions.cmake
@@ -1,9 +1,11 @@
 add_library(disable_exceptions INTERFACE)
 
-target_compile_options(disable_exceptions INTERFACE
-    -sDISABLE_EXCEPTION_CATCHING
-    -fno-exceptions
-)
-target_link_options(disable_exceptions INTERFACE
-    -sDISABLE_EXCEPTION_CATCHING
-)
+if(WASM_DOM_NO_EXCEPTION)
+    target_compile_options(disable_exceptions INTERFACE
+        -sDISABLE_EXCEPTION_CATCHING
+        -fno-exceptions
+    )
+    target_link_options(disable_exceptions INTERFACE
+        -sDISABLE_EXCEPTION_CATCHING
+    )
+endif()
--- cmake/optimization_flags.cmake
+++ cmake/optimization_flags.cmake
@@ -1,19 +1,21 @@
 add_library(optimization_flags INTERFACE)
 
-target_compile_definitions(optimization_flags INTERFACE
-    EMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0
-)
-target_compile_options(optimization_flags INTERFACE
-    $<$<CONFIG:Release>:-O3>
-    -fno-rtti
-)
-target_link_options(optimization_flags INTERFACE
-    $<$<CONFIG:Release>:--closure 1>
-    -fno-rtti
-)
-
-if(NOT WASM_DOM_COVERAGE)
-    target_link_options(optimization_flags INTERFACE
-        -sNO_FILESYSTEM
+if(WASM_DOM_FULL_OPTIMISATION)
+    target_compile_definitions(optimization_flags INTERFACE
+        EMSCRIPTEN_HAS_UNBOUND_TYPE_NAMES=0
     )
+    target_compile_options(optimization_flags INTERFACE
+        $<$<CONFIG:Release>:-O3>
+        -fno-rtti
+    )
+    target_link_options(optimization_flags INTERFACE
+        $<$<CONFIG:Release>:--closure 1>
+        -fno-rtti
+    )
+
+    if(NOT WASM_DOM_COVERAGE)
+        target_link_options(optimization_flags INTERFACE
+            -sNO_FILESYSTEM
+        )
+    endif()
 endif()
